1fb179731aa5fbbc0d9e509238dee1bdf8f80aed
Tobias Nguyen <dinoliu570@Gmail.com>
2025-09-07 23:21:48 -0300

    Agregando funcionalidad final

--- C++/Laboratorio/TP VECTORES/main.cpp
+++ C++/Laboratorio/TP VECTORES/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     srand(time(0));
     /*
-    Comando para compilar clang++ -std=c++14 main.cpp funciones.cpp -o main
+    clang++ -std=c++14 main.cpp funciones.cpp -o main
     */
 
     int AlimentosRecolectados[12];
@@ -15,19 +15,16 @@ int main() {
     float promedio;
     bool Descalificados[12] = {false};
 
-    // Generacion de Alimentos Aleatorios
     ingresarAlimentos(AlimentosRecolectados,12);
-
-    // Generacion de el tiempo empleado aleatorio
     ingresarTiempoEmpleado(TiempoEmpleado,12);
-
-    // Evaluación de descalificación
     evaluarDescalificados(TiempoEmpleado, AlimentosRecolectados, Descalificados, 12);
 
-    // Mas rapido en la Construccion
     int ParticipanteMasRapido = MasRapidoTiempoConstruccion(TiempoEmpleado, 12);
     int minTiempo = TiempoEmpleado[ParticipanteMasRapido];
 
+    cout << "===== MENU PRINCIPAL =====\n";
+    cout << "Presione la tecla 'ENTER' para mostrar resultados..." << endl;
+    cin.get();
 
     cout << "\n========== RESULTADOS ==========\n";
 
@@ -36,15 +33,12 @@ int main() {
     promedio = CalcularPromedioAlimentos(AlimentosRecolectados,12);
     cout << "\nPromedio de alimentos recolectados: " << promedio << " Kg\n";
 
-    
     cout << "Participantes por debajo del promedio:\n";
     Debajo_Promedio(AlimentosRecolectados,12,promedio);
 
-    
     cout << "\nEl participante mas rapido fue el " << ParticipanteMasRapido + 1 << " con " << minTiempo << " dias.\n";
     
     cout << "La cantidad de participantes que no llegaron a completar su refugio es: " << NoCompletaronRefugio(TiempoEmpleado,12) << endl;
-    
 
     return 0;
 }
