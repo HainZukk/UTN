3742f404e7261c6ebbe58b830be8ea689d918236
Tobias Nguyen <dinoliu570@Gmail.com>
2025-10-23 18:40:26 -0300

    Implementacion de funcion

--- C++/E7 Ejercicios/01-Ejr.cpp
+++ C++/E7 Ejercicios/01-Ejr.cpp
@@ -1,18 +1,29 @@
+/*
+Hacer un programa para ingresar 10 números y guardarlos en un vector. Determinar e informar cuál es la suma de los valores del vector.
+*/
+
 #include <iostream>
 using namespace std;
-int main(){
-    
-    //Declaramos el vector / array en 10
-    int Numeros[10];
+
+void SumaValores(int Nums[10]){
+
     int suma = 0;
 
     for (int i = 0; i < 10; i++){
-    cout << "Ingrese el numero " << i + 1 << ": ";
-    cin >> Numeros[i]; 
-    suma += Numeros[i];
+        cout << "Ingrese el numero" << i + 1 << " : " << endl;
+        cin >> Nums[i];
+        suma += Nums[i];
     }
+    cout << "La suma del vector es de: " << suma << endl;
 
-    cout << "La suma de los numeros en el vectores es: " << suma << endl;
-    
+}
+
+int main(){
+
+    int suma;
+    int Nums[10];
+
+    SumaValores(Nums);
+
     return 0;
 }
