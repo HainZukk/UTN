336e876bb654b1ce9b044f0c543784b078441161
Tobias Nguyen <dinoliu570@Gmail.com>
2026-04-18 05:09:32 -0300

    fix: Arreglando ejercicio (en vez de cargar estaba acumulando)

--- C++/Laboratorio/Cargar_Arreglo.cpp
+++ C++/Laboratorio/Cargar_Arreglo.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void Cargar_Arreglo(int Mi_Arreglo[]){
     for (int i = 1; i < 15; i++){
-        Mi_Arreglo[i] += i;
+        Mi_Arreglo[i] = i;
     }
 }
 
@@ -11,7 +11,7 @@ int main (){
     // Inicializamos en 0 el Array/Vector porque si no toma valores del espacio de Memoria
     int mi_array[15] = {0};
     Cargar_Arreglo(mi_array);
-    for (int i = 1; i < 15; i++) {
+    for (int i = 0; i < 15; i++) {
         cout << mi_array[i] << " ";
     }
     return 0;
