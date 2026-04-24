03a9547a925c76b218d100b92b5360f50bbb8a40
Tobias Nguyen <dinoliu570@Gmail.com>
2026-04-24 05:25:05 -0300

    fix: Espacio en la opcion numero 3

--- C++/Laboratorio/TP_Survival/funciones.cpp
+++ C++/Laboratorio/TP_Survival/funciones.cpp
@@ -23,7 +23,7 @@ void menuPrincipal() {
                 jugar();
                 break;
             case 2:
-                cout << "Creditos - Nguyen Tobias - Legajo 32580";
+                cout << "Creditos - Nguyen Tobias - Legajo 32580" << endl;
                 break;
             case 3:
                 cout << "Saliendo del juego..." << endl;
@@ -32,7 +32,7 @@ void menuPrincipal() {
                 cout << "Opción inválida." << endl;
                 break;
         }
-    }while (opcion != 3);
+    } while (opcion != 3);
     
 }
 
