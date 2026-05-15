11fdbe014c842a72b7fa7d6383a06c4464575837
Tobias Nguyen <dinoliu570@Gmail.com>
2026-05-15 20:09:15 -0300

    (new): declaracion de tipos de datos de nuevas funciones

--- C++/TP-LosCincosDemonios/funciones.h
+++ C++/TP-LosCincosDemonios/funciones.h
@@ -1,11 +1,17 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
+
 #include <string>
+
 using namespace std;
 
 void menuPrincipal();
+
 void jugar();
-void mostrarEstadoDeSigilos(bool estado_sigilos[]);
+
+void mostrarEstadoDeSigilos(bool estado_sigilos[],const string NomDemonios[],const string ElementosDemonios[],int cantDemonios);
+int tirarDado();
+void EvaluarTirada(int dado1,int dado2,bool demonios_disponibles[] , int cantDemonios)
 
 
 #endif
