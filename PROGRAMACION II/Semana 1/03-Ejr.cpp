c4f749d9bc250db8d17afb293f8f224ffbf0f364
Tobias Nguyen <dinoliu570@Gmail.com>
2026-08-14 14:07:12 -0300

    new: Terminando el ejercicio 03 - falta validacion de dias

--- PROGRAMACION II/Semana 1/03-Ejr.cpp
+++ PROGRAMACION II/Semana 1/03-Ejr.cpp
@@ -1,10 +1,111 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+
+
+int mayorGasto(float totalPorCategoria[],int MAX_CAT){
+
+    int mayorIndice = 0;
+    for (int i = 0; i < MAX_CAT; i++){
+        if (totalPorCategoria[i] > totalPorCategoria[mayorIndice]){
+            mayorIndice = i;
+        }
+    }
+    return mayorIndice;
+}
+
+void mostrarPorCategoria(float totalPorCategoria[],string nombresCategorias[],int MAX_CAT){
+    for (int i = 0; i < MAX_CAT; i++){
+        cout << nombresCategorias[i] << ":  $" << totalPorCategoria[i] << endl;
+    }
+}
+
+// Categorias sin movimientos
+int catSinMovimientos(float totalPorCategoria[],int MAX_CAT){
+    int contadorSinMov = 0;
+
+    for (int i = 0; i < MAX_CAT; i++){
+        if (totalPorCategoria[i] == 0){
+            contadorSinMov++;
+        }
+    }
+    return contadorSinMov;
+}
+
+void gastoPorDia(int cantidadPorDia[],int MAX_DIAS){
+    cout << "Gastos por dia: \n";
+    for (int i = 0; i < MAX_DIAS; i++){
+        if (cantidadPorDia[i] > 0){
+            cout << "Dia " << i + 1 << ": " << cantidadPorDia[i] << " gasto(s)" << endl;
+        }
+    }
+}
+
+
 int main(){
 
-    
+    const int MAX_CAT = 10;
+    const int MAX_DIAS = 31;
+    int dia , categoria;
+    float importe;
 
 
+
+    string nombresCategorias[MAX_CAT] = {
+        "Servicios", "Alimentacion", "Limpieza", "Transporte", "Educacion",
+        "Salud", "Ocio", "Impuestos", "Vestimenta", "Inversiones"
+    };
+
+    float totalPorCategoria[MAX_CAT] = {0};
+    int cantidadPorDia[MAX_DIAS] = {0};
+
+    cout << "Ingrese el dia (0 para finalizar)" << endl;
+    cin >> dia;
+
+    while (dia != 0){
+        do {
+            cout << "Ingrese la categoria (1-10): ";
+            cin >> categoria;
+
+            if (categoria < 1 || categoria > 10){
+                cout << "Ingrese una categoria en el rango anunciado" << endl;
+            }
+            
+        } while (categoria < 1 || categoria > 10);
+
+        cout << "Ingrese el importe del dia: " << endl;
+        cin >> importe;
+
+        totalPorCategoria[categoria-1] += importe; // por ejemplo si la categoria es 1 el indice es 0
+        cantidadPorDia[dia-1]++;
+
+
+        cout << "Ingrese el dia (0 para finalizar)" << endl;
+        cin >> dia;
+    }
+
+    /*
+    A) La categoría de gasto que mayor dinero se haya destinado y cuál es dicha categoría.
+    */
+
+    int indiceMayor = mayorGasto(totalPorCategoria,MAX_CAT);
+    cout << "\n--- A) Categoria con mayor gasto ---\n";
+    cout << nombresCategorias[indiceMayor] << ": $" << totalPorCategoria[indiceMayor] << endl; 
+
+    /*
+    B) Total por categoria
+    */
+    mostrarPorCategoria(totalPorCategoria,nombresCategorias,MAX_CAT);
+
+
+    // C
+
+    int sinMovimientos = catSinMovimientos(totalPorCategoria,MAX_CAT);
+    cout << "Cantidad de categorias sin movimientos es: " << sinMovimientos << endl;
+
+    // D Gastos por Dia
+    gastoPorDia(cantidadPorDia,MAX_DIAS);
 
     return 0;
 }
