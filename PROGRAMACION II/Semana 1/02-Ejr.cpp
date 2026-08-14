5af4a4ba4988c562266055f80781a52fa8e7d794
Tobias Nguyen <dinoliu570@Gmail.com>
2026-08-14 08:09:23 -0300

    new: Terminando el ejercicio (bandera,contadores,modularizacion)

--- PROGRAMACION II/Semana 1/02-Ejr.cpp
+++ PROGRAMACION II/Semana 1/02-Ejr.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 using namespace std;
 
+int quitarRepetidos(int vecSinProcesar[],int vecProcesado[], int TAM){
 
-void quitarRepetidos(int vecSinProcesar[],int vecProcesado, int TAM){
+    int contador = 0;
 
     for (int i = 0; i < TAM; i++){
-        for (int j = 0; i < TAM; j++){
-            if (vecSinProcesar[i] != vecSinProcesar[j + 1]){
-                
-            }
-            
-        }
-        
-    }
+        bool yaExiste = false;
 
+        for (int j = 0; j < contador; j++){
+            if (vecSinProcesar[i] == vecProcesado[j]){
+                yaExiste = true;
+                break;
+            } 
+        }
+        if (!yaExiste){
+            vecProcesado[contador] = vecSinProcesar[i];
+            contador++;
+        }
+    }
+    return contador;
+}
+
+void mostrarElementosVector(int vecProcesado[],int TAM){
+    for (int i = 0; i < TAM; i++){
+        cout << vecProcesado[i] << endl;
+    }
+    cout << endl;
 }
 
 
@@ -21,8 +34,14 @@ void quitarRepetidos(int vecSinProcesar[],int vecProcesado, int TAM){
 int main(){
 
     const int TAM = 10;
-    int vectorSinProcesar[TAM] = { 1, 2, 1, 2, 5, 5, 4, 4, 3, 3 };
+    int vectorSinProcesar[TAM] = {1,2,1,2,5,5,4,4,3,3};
     int vectorSinRepetidos[TAM];
+
+    int elementos = quitarRepetidos(vectorSinProcesar,vectorSinRepetidos,TAM);
+    cout << "Elementos sin repetir: " << elementos << endl;
+    cout << "Vector sin Repetidos: " << endl;
+    mostrarElementosVector(vectorSinRepetidos,elementos);
+
 
     return 0;
 }
